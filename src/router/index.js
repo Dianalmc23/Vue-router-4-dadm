0a6174e9a66027f2b92eb9ddafa5fa0e5e4dfe7c
Dianalmc23 <93403544+Dianalmc23@users.noreply.github.com>
2023-11-22 18:49:10 +0000

    ⭐Feat: Assets & Brazil

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     }
+    {
+    path: '/brazil',
+    name: '/brazil',
+    component: () => import('../views/BrazilView.vue')
+    }
   ]
 })
 
